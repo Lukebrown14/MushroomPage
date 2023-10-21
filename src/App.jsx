98443c173e40ac5b0617615f65a34d5a97f91518
Luke Brown <lukebrown14@hotmail.com>
2023-10-21 17:25:58 +0000

    Crete React App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './component/header/Header';
+import Body from './component/header/body/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
