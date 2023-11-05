5f5c006e3e0cc5a902c24c7dc3f4fa7d0ecdcbe1
Luke Brown <lukebrown14@hotmail.com>
2023-11-05 20:11:23 +0000

    Updated the header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-import './header.css'
-
+import "./header.css";
 
 const Header = () => {
-    return <h1 className="Title">Mushroom Page</h1>
-}
+  return (
+    <div class="Header">
+      <div class="Author">
+        <h2> LUKE BROWN</h2>
+      </div>
+      <div class="fungiTitle">
+        <h1 className="Title">Fungi</h1>
+      </div>
+      <div class="learnTitle">
+        <h1 className="Learn"> Learn The Mushroom</h1>
+      </div>
+      <div class="div4"> </div>
+      <div class="div5">
+        <img src="Mushroom.png" alt="Mushroom Drawing" />
+      </div>
+      <div class="BeTitle">
+        <h1 className="Be"> Be The Mushroom</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
